bfb3eb5200a990568da645e53ba5ae03c8c7bd4f
Yuji Nakayama <nkymyj@gmail.com>
2013-11-08 00:58:46 +0900

    Improve rbenv provisioning script

--- provision/rbenv.sh
+++ provision/rbenv.sh
@@ -28,7 +28,31 @@ fi
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+
+default_gems_path="$HOME/.rbenv/default-gems"
+
+cat <<END > "$default_gems_path"
+bundler
+guard-rspec
+guard-rubocop
+pry-debugger
+rails
+sinatra
+END
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+cat <<END >> "$default_gems_path"
+cocoapods
+nomad-cli
+ruby_gntp
+END
+fi
 
 latest_2_0="$(basename "$(ls ~/.rbenv/plugins/ruby-build/share/ruby-build/2.0.* | egrep -v 'dev|pre|rc' | sort | tail -1)")"
 
 rbenv install "$latest_2_0"
+rbenv global "$latest_2_0"
+gem update --system
+gem clean
