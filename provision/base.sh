9714186d5c24dcdb6c8796c492972a415d9cad0c
Yuji Nakayama <nkymyj@gmail.com>
2013-11-20 07:50:39 +0900

    Use z bundled with oh-my-zsh

--- provision/base.sh
+++ provision/base.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$BASH_SOURCE")"
 ./prepare.sh || exit 1
 
 if [[ $(uname -s) == 'Darwin' ]]; then
-  brew install emacs git tree z zsh zsh-completions
+  brew install emacs git tree zsh zsh-completions
   brew install --HEAD screen
 else
   sudo apt-get install --yes emacs24-nox git screen tree zsh
--- zshrc.d/z.zsh
+++
@@ -1,5 +0,0 @@
-
-# Enable z installed with Homebrew.
-if command_exist brew && [[ -e "$(brew --prefix)/etc/profile.d/z.sh" ]]; then
-  source "$(brew --prefix)/etc/profile.d/z.sh"
-fi
