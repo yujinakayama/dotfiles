153255852820f2c04a8ee59746136b456105dc4c
Yuji Nakayama <nkymyj@gmail.com>
2016-02-24 21:06:10 +0900

    Bind shift-tab for moving completion list backward in zsh

--- zshrc.d/keybind.zsh
+++ zshrc.d/keybind.zsh
@@ -6,6 +6,9 @@ bindkey -e
 bindkey '^[\e[C' forward-word
 bindkey '^[\e[D' backward-word
 
+# Bind shift-tab for moving completion list backward.
+bindkey '^[[Z' reverse-menu-complete
+
 # Override history-incremental-search-backward with
 # history-incremental-pattern-search-backward that allows to search history with pattern.
 # Also add forward search.
