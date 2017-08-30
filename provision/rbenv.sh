96b3bd65fc08b9506cbd57caa21a73be5a16cef2
Yuji Nakayama <nkymyj@gmail.com>
2017-08-30 13:17:49 +0900

    Use GitHub rbenv organization for cloning rbenv repos

--- provision/rbenv.sh
+++ provision/rbenv.sh
@@ -26,8 +26,8 @@ fi
 if [[ -d "$HOME/.rbenv" ]]; then
   rbenv update
 else
-  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
 
   export PATH="$HOME/.rbenv/bin:$PATH"
