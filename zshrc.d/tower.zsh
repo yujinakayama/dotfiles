6d41a8da1d9d544317654917fdc9d67588c6e1fc
Yuji Nakayama <nkymyj@gmail.com>
2019-06-01 19:44:40 +0900

    Improve tower command

--- zshrc.d/tower.zsh
+++ zshrc.d/tower.zsh
@@ -1,6 +1,18 @@
 if command_exist gittower; then
   # Convenience way to open the current project with Tower.app.
   tower() {
-    gittower .
+    directory="$(pwd)"
+
+    until [ "$directory" = "/" ]; do
+      if [ -d "$directory/.git" ]; then
+        gittower "$directory"
+        return 0
+      fi
+
+      directory="$(dirname "$directory")"
+    done
+
+    echo "Error: No git directory found in the ancestor directories" >&2
+    return 1
   }
 fi
