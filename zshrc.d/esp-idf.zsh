cf6f4e611699df602f095244ded1cba8f7ef660a
Yuji Nakayama <nkymyj@gmail.com>
2020-10-10 21:59:39 +0900

    Use xtensa-esp32-elf installed by ./install.sh of the latest ESP-IDF

--- zshrc.d/esp-idf.zsh
+++ zshrc.d/esp-idf.zsh
@@ -1,6 +1,9 @@
 # https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html
 # https://docs.espressif.com/projects/esp-idf/en/latest/get-started/macos-setup.html
 if [[ -d "$HOME/.esp/esp-idf/versions" ]]; then
-  path=("$HOME/.esp/xtensa-esp32-elf/bin" $path)
   export IDF_PATH="$HOME/.esp/esp-idf/versions/3.3.4"
+fi
+
+if [[ -d "$HOME/.espressif" ]]; then
+  path=("$HOME/.espressif/tools/xtensa-esp32-elf" $path)
 fi
