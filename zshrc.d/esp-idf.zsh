ff9a311cf1eb16e15055b0bf09f6bf4ddba32c3c
Yuji Nakayama <nkymyj@gmail.com>
2020-10-11 04:19:45 +0900

    Support esp-idf master version

--- zshrc.d/esp-idf.zsh
+++ zshrc.d/esp-idf.zsh
@@ -4,3 +4,10 @@ if [[ -d "$HOME/.esp-idf/versions/3.2" ]]; then
     export IDF_PATH="$HOME/.esp-idf/versions/3.2/esp-idf"
   }
 fi
+
+if [[ -d "$HOME/.esp-idf/versions/master" ]]; then
+  function esp_idf_master() {
+    export IDF_TOOLS_PATH="$HOME/.esp-idf/versions/master/tools"
+    . "$HOME/.esp-idf/versions/master/esp-idf/export.sh"
+  }
+fi
