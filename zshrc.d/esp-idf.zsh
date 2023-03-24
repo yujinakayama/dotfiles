2efbb09585e6f796ff8256ec4eee2ccfcb68dc66
Yuji Nakayama <nkymyj@gmail.com>
2023-03-24 20:28:15 +0900

    Add esp_idf_4.3

--- zshrc.d/esp-idf.zsh
+++ zshrc.d/esp-idf.zsh
@@ -5,9 +5,16 @@ if [[ -d "$HOME/.esp-idf/versions/3.2" ]]; then
   }
 fi
 
+if [[ -d "$HOME/.esp-idf/versions/4.3" ]]; then
+  function esp_idf_4.3() {
+    export IDF_TOOLS_PATH="$HOME/.esp-idf/versions/4.3"
+    . "$HOME/.esp-idf/versions/4.3/esp-idf/export.sh"
+  }
+fi
+
 if [[ -d "$HOME/.esp-idf/versions/master" ]]; then
   function esp_idf_master() {
-    export IDF_TOOLS_PATH="$HOME/.esp-idf/versions/master/tools"
+    export IDF_TOOLS_PATH="$HOME/.esp-idf/versions/master"
     . "$HOME/.esp-idf/versions/master/esp-idf/export.sh"
   }
 fi
